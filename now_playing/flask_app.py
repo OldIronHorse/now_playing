369d27c34473785acfecbabe9a2db157d49a499b
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-12-20 11:11:52 +0000

    Query player by name

--- now_playing/flask_app.py
+++ now_playing/flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from juice import connect, get_players
+from juice import connect, get_players, get_playing_track
 
 app = Flask('now_playing')
 
@@ -20,3 +20,10 @@ def players():
 def players_count():
   server = connect('euterpe3')
   return str(len(get_players(server)))
+
+@app.route('/players/<name>')
+def player_by_name(name):
+  server = connect('euterpe3')
+  players = [player for player in get_players(server) if player.name == name]
+  track = get_playing_track(server,players[0].id)
+  return '{}, {}, {}'.format(track.title,track.album,track.artist)
