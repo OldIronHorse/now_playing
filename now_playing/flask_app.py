bc8c29f5d28c89e78aa5419dcf4ec27ebc36dac8
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-12-20 13:19:20 +0000

    basic artists query added

--- now_playing/flask_app.py
+++ now_playing/flask_app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from juice import connect, get_players, get_playing_track
+from flask import Flask, jsonify
+from juice import connect, get_players, get_playing_track, get_artists
 
 app = Flask('now_playing')
 
@@ -11,6 +11,7 @@ def hello_world():
 def players():
   server = connect('euterpe3')
   players = get_players(server)
+  server.close()
   response = '';
   for player in players:
     response += '{}, {}\n'.format(player.name,player.id)
@@ -19,11 +20,22 @@ def players():
 @app.route('/players/count')
 def players_count():
   server = connect('euterpe3')
-  return str(len(get_players(server)))
+  players = get_players(server)
+  server.close()
+  return str(len(players))
 
-@app.route('/players/<name>')
+@app.route('/players/<name>/now_playing')
 def player_by_name(name):
   server = connect('euterpe3')
   players = [player for player in get_players(server) if player.name == name]
   track = get_playing_track(server,players[0].id)
+  server.close()
   return '{}, {}, {}'.format(track.title,track.album,track.artist)
+
+@app.route('/library/artists')
+def library_artists():
+  server = connect('euterpe3')
+  artists = get_artists(server)
+  server.close()
+  #TODO: make the json presentation nice
+  return jsonify(artists)
