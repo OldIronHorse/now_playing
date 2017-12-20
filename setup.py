81635a41538d460deb8159d8d7de978940c143cb
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-12-20 10:16:10 +0000

    Basic project structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(name='now_playing',
+      version='0.1',
+      description='A web service frontend to the Squeezebox CLI',
+      author='OldIronHorse',
+      author_email='',
+      license='GPL 3.0',
+      packages=['now_playing'],
+      install_requires=[
+        'flask'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
