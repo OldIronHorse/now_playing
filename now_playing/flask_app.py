f418159920a2378990aa04633b5ec4540cb18d79
Simon Rdding <s1m0n.r3dd1ng@gmail.com>
2018-01-04 10:34:09 +0000

    login and basic player list working

--- now_playing/flask_app.py
+++ now_playing/flask_app.py
@@ -1,15 +1,48 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, session, redirect, url_for, flash
 from juice import connect, get_players, get_playing_track, get_artists
 
-app = Flask('now_playing')
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+  SECRET_KEY='develeopment key',
+  SERVER='euterpe3',
+  USERNAME='default',
+  PASSWORD='password'))
+app.config.from_envvar('NOW_PLAYING_SETTINGS', silent=True)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def show_players():
+  server = connect(app.config['SERVER'])
+  players = get_players(server)
+  server.close()
+  return render_template('show_players.html', players=players)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+  error = None
+  if request.method == 'POST':
+    print(repr(request.form))
+    if request.form['username'] != app.config['USERNAME'] \
+        or request.form['password'] != app.config['PASSWORD']:
+      error = 'Invalid credentials'
+    else:
+      session['logged_in'] = True
+      flash('You are logged in')
+      return redirect(url_for('show_players'))
+  return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+  session.pop('logged_in',None)
+  flash('You were logged out')
+  return redirect(url_for('show_players'))
+
+### Start service POC ###
 
 @app.route('/players')
 def players():
-  server = connect('euterpe3')
+  server = connect(app.config['SERVER'])
   players = get_players(server)
   server.close()
   response = '';
@@ -19,14 +52,14 @@ def players():
   
 @app.route('/players/count')
 def players_count():
-  server = connect('euterpe3')
+  server = connect(app.config['SERVER'])
   players = get_players(server)
   server.close()
   return str(len(players))
 
 @app.route('/players/<name>/now_playing')
 def player_by_name(name):
-  server = connect('euterpe3')
+  server = connect(app.config['SERVER'])
   players = [player for player in get_players(server) if player.name == name]
   track = get_playing_track(server,players[0].id)
   server.close()
@@ -34,10 +67,13 @@ def player_by_name(name):
 
 @app.route('/library/artists')
 def library_artists():
-  server = connect('euterpe3')
+  server = connect(app.config['SERVER'])
   artists = get_artists(server)
   server.close()
   return jsonify([artist._asdict() for artist in artists])
+
+#TODO:html interface
+#TODO:js interface?
 
 #TODO: cache library at startup and cache in python dictionaries
 #TODO: index library cache
